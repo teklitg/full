7b9a547ddb9c3a6f0622f709035a00281cd0c453
Teklit Gebrerufael <teklit.gebrer@gmail.com>
2023-11-21 03:15:02 -0800

    database integrated

--- mongo.js
+++ mongo.js
@@ -7,25 +7,35 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://<username>:${password}@cluster0.ygyu7gw.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://teklit:${password}@cluster0.80htwfy.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is Easy',
+  content: 'I will be a hacker',
+  date: new Date(),
   important: true,
 })
 
+
 note.save().then(result => {
   console.log('note saved!')
+  mongoose.connection.close()
+})
+
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
